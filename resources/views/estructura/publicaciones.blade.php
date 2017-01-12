1693c87fcd46fbfc2dc437ebd625d5c5e57b8bdf
mnovoaq <mitchel.novoa@gmail.com>
2017-01-12 19:13:10 -0300

    correcciones dashboard

--- resources/views/estructura/publicaciones.blade.php
+++ resources/views/estructura/publicaciones.blade.php
@@ -4,25 +4,7 @@
 	<div class="col-lg-9">
 		<strong>Nullam efficitur nisl in massa ultricies</strong>
 		<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
-	</div><br><br>
-	<div class="col-lg-3"><br><img src="http://entreligas.cl/wp-content/uploads/2016/12/Captura-de-pantalla-2016-12-15-a-las-18.14.50-611x300.png" alt="" width="160"></div>
-	<div class="col-lg-9">
-		<strong>Nullam efficitur nisl in massa ultricies</strong>
-		<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
-	</div><br><br>
-	<div class="col-lg-3"><br><img src="http://entreligas.cl/wp-content/uploads/2016/12/Captura-de-pantalla-2016-12-15-a-las-18.14.50-611x300.png" alt="" width="160"></div>
-	<div class="col-lg-9">
-		<strong>Nullam efficitur nisl in massa ultricies</strong>
-		<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
-	</div><br><br>
-	<div class="col-lg-3"><br><img src="http://entreligas.cl/wp-content/uploads/2016/12/Captura-de-pantalla-2016-12-15-a-las-18.14.50-611x300.png" alt="" width="160"></div>
-	<div class="col-lg-9">
-		<strong>Nullam efficitur nisl in massa ultricies</strong>
-		<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
-	</div><br><br>
-	<div class="col-lg-3"><br><img src="http://entreligas.cl/wp-content/uploads/2016/12/Captura-de-pantalla-2016-12-15-a-las-18.14.50-611x300.png" alt="" width="160"></div>
-	<div class="col-lg-9">
-		<strong>Nullam efficitur nisl in massa ultricies</strong>
-		<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
-	</div><br><br>
+	</div>
+
+
 </div>
