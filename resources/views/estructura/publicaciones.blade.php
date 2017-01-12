531bb4c50e71a8930aa6af02c0c38f8e77bc1716
mnovoaq <mitchel.novoa@gmail.com>
2017-01-12 16:38:10 -0300

    corrección dashboard

--- resources/views/estructura/publicaciones.blade.php
+++ resources/views/estructura/publicaciones.blade.php
@@ -1,4 +1,5 @@
-<div class=" columna-public">
+<div class="col-lg-2"></div>
+<div class="col-lg-8 columna-public">
 	<p>Estimado (a) Mitchel
  
 SolucionHost se enorgullece en ofrecer a usted la más amplia y vasta plataforma de documentación en español existente en la actualidad, como empresa hemos comprendido y estudiado las diferentes problemáticas más recurrentes en nuestra comunidad de usuarios y en base a esto se han desarrollado distintos tutoriales, manuales, videos, entre otros, todos en pro de solucionar de forma rápida y efectiva cada una de sus dudas y/o inconvenientes.
@@ -12,3 +13,4 @@ Para acceder a nuestra plataforma ingresa desde tu navegador de preferencia a: h
 Una vez en el portal solo debes escribir palabras claves asociadas a tu consulta, por ejemplo, si deseas obtener información de como configurar tus DNS solo bastaría con escribir “configurar dns” y se mostrará una lista de resultados, en donde, el primer resultado es el que posee mayores coincidencias y de seguro es la respuesta a tu duda.
 </p>
 </div>
+<div class="col-lg-2"></div>
---
+++ resources/views/estructura/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="rightbar" id="sidebar">
+		<img class="brand" src="EntreLigas.png" alt="" width="150">
+		<div class="photofile">
+			<img class="img-circle" src="player.jpg" alt="" width="80" height="80">			
+		</div>
+		<div class="nametitle" align="center">
+			<span class="upper">Sergio Chaves</span><br>
+			<span class="lowcasse">Perfil jugador </span>
+			<hr>				
+		</div>
+		<div class="contentbar" >
+			<ul>
+				<li>
+					<a href="#">
+						<i class="fa fa-user"></i><span>Información</span>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<i class="fa fa-bar-chart" aria-hidden="true"></i><span>Estadísticas</span>
+					</a>
+				</li><li>
+					<a href="#">
+						<i class="fa fa-envelope" aria-hidden="true"></i><span>Mensajes</span>
+					</a>
+				</li><li>
+					<a href="#">
+						<i class="fa fa-shield" aria-hidden="true">&nbsp;</i><span>Equipo</span>
+					</a>
+				</li><li>
+					<a href="#">
+						<i class="fa fa-futbol-o" aria-hidden="true"></i><span>Fechas</span>
+					</a>
+				</li>	
+			</ul>
+			
+		</div>			
+	</div>
