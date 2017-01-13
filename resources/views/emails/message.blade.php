b652dd77e31cafeb2317e6f7e6f1999979068888
rekkiem7 <pbarria.reyes@gmail.com>
2017-01-13 17:28:14 -0300

    correccion

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -69,7 +69,7 @@ Mensaje recibido
 <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
 <tr style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
 <td class="content-block" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-<strong>De : </strong> {{ $email }}
+<strong>De : </strong> info@entreligas.cl
 </td>
 </tr>
                            <tr style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
