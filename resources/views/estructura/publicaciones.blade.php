db72e682ff64a889d23a3b01633cc765fb06b73a
rekkiem7 <pbarria.reyes@gmail.com>
2017-01-13 10:06:04 -0300

    ventanas publicaciones

--- resources/views/estructura/publicaciones.blade.php
+++ resources/views/estructura/publicaciones.blade.php
@@ -1,10 +1,15 @@
 
 <div class="col-lg-8 columna-public animated zoomInUp"><br>
-	<div class="col-lg-3"><br><img src="http://entreligas.cl/wp-content/uploads/2016/12/Captura-de-pantalla-2016-12-15-a-las-18.14.50-611x300.png" alt="" width="160"></div>
-	<div class="col-lg-9">
-		<strong>Nullam efficitur nisl in massa ultricies</strong>
-		<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
-	</div>
-
+	<div class="box box-danger">
+        <div class="box-header with-border">
+          <h3 class="box-title"><strong>Nullam efficitur nisl in massa ultricies</strong></h3>
+        </div>
+        <div class="box-body">
+			<div class="col-lg-3"><br><img src="http://entreligas.cl/wp-content/uploads/2016/12/Captura-de-pantalla-2016-12-15-a-las-18.14.50-611x300.png" alt="" width="160"></div>
+			<div class="col-lg-9">
+				<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut fermentum ante. Suspendisse sed auctor nunc. Cras luctus, ipsum commodo rhoncus facilisis, turpis magna blandit metus, eget feugiat ex massa quis lacus. Praesent nec velit eu lacus ullamcorper efficitur in in augue. </p>
+			</div>
+		</div>	
+	</div>	
 
 </div>
