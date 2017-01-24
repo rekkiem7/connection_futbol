fc9dc56c907f90ef0c6d2b7587c67e2fa482a0b1
rekkiem7 <pbarria.reyes@gmail.com>
2017-01-24 12:15:39 -0300

    Ennvio de email listo

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -37,6 +37,8 @@ class MailController extends Controller
     {
            try{
               $data = $request->all();
+              $imagen1=url('/').'/EntreLigas.png';
+              $teamEscude=url('/').'/'.$request->teamEscude;
              //se envia el array y la vista lo recibe en llaves individuales {{ $email }} , {{ $subject }}...
                if($request->type==1)
                {
@@ -45,18 +47,24 @@ class MailController extends Controller
                  $plantilla='emails.templates.invitationPlayer';  
                }
 
-               $view = View::make($plantilla)->with($data);
-               $contents = $view->render();//contenido del mail en string
-               $email=Mail::send($plantilla, $data, function($message) use ($request)
+               //$view = View::make($plantilla)->with($data);
+               //$contents = $view->render();//contenido del mail en string>
+               Mail::send($plantilla, ['imagen1' => $imagen1,'email'=>$request->email,'subject'=>$request->subject,'teamEscude'=>$teamEscude,'teamName'=>$request->teamName,'leagueName'=>$request->leagueName,'categoryName'=>$request->categoryName,'body'=>$request->body,'league'=>$request->league,'category'=>$request->category,'tournament'=>$request->tournament,'team'=>$request->team,'type'=>$request->type], function ($message) use ($request) {
+                            $message->from('info@entreligas.cl','EntreLigasFC');
+                            $message->subject($request->subject);
+                            $message->to($request->email,$request->name);
+                        });
+                       
+               /*$email=Mail::send($plantilla, $data, function($message) use ($request)
                {
                    //remitente
                    $message->from('info@entreligas.cl','EntreLigasFC');
                    $message->subject($request->subject);
                    $message->to($request->email, $request->name);
-               });
+               });*/
                return 1;
            }catch(\Exception $e){
-              echo "aqui";
+              dd($e);
            }
     }
 }
